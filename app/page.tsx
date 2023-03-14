d2f3113c7712d34451def8c743a3e2d7ca6e27a6
Vilyard <darko.stojanovski64@gmail.com>
2023-03-14 13:59:25 +0100

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,34 +1,32 @@
 "use client";
 import { useState } from "react";
-import { Button, Divider, List, ListItem, ListItemText, TextField } from "@mui/material";
+import { Button, TextField } from "@mui/material";
+import ListData from "../components/ListData";
 
 export default function Home() {
   const [cityInp, setCityInp] = useState("");
   const [weatherData, setWeatherData] = useState<any>({});
-  const style = {
-    width: '100%',
-    maxWidth: 360,
-    bgcolor: 'background.paper',
-    marginLeft:"770px"
-  };
+
   async function getWeatherData() {
     try {
       const res = await fetch(
         `https://api.openweathermap.org/data/2.5/weather?q=${cityInp}&appid=${process.env.NEXT_PUBLIC_API_KEY}&units=metric`
       );
-      console.log(res)
+      console.log(res);
       const data = await res.json();
       if (data?.cod === "400") throw data;
-      
+
       setWeatherData(data);
     } catch (err) {
       console.log(err);
     }
   }
   return (
-    <div style={{
-     textAlign:"center"
-    }}>
+    <div
+      style={{
+        textAlign: "center",
+      }}
+    >
       <div>
         <TextField
           variant="standard"
@@ -36,26 +34,13 @@ export default function Home() {
           id="standard-basic"
           onChange={(e) => setCityInp(e.target.value)}
         />
-        <Button variant="contained" onClick={() => getWeatherData()}>Get weather!</Button>
+        <Button variant="contained" onClick={() => getWeatherData()}>
+          Get weather!
+        </Button>
       </div>
       {Object.keys(weatherData).length !== 0 ? (
         <>
-          <List sx={style} component="nav" aria-label="mailbox folders">
-            <ListItem button>
-              <ListItemText primary={weatherData.id} secondary="City ID" />
-            </ListItem>
-            <Divider />
-            <ListItem button divider>
-              <ListItemText primary={weatherData.name} secondary="City Name"/>
-            </ListItem>
-            <ListItem button>
-              <ListItemText primary={weatherData.main.temp} secondary="City Temp &#8451;" />
-            </ListItem>
-            <Divider light />
-            <ListItem button>
-              <ListItemText primary={weatherData.timezone}  secondary="City Timezone"/>
-            </ListItem>
-          </List>
+          <ListData props={weatherData} />
         </>
       ) : null}
     </div>
---
+++ components/ListData.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { Divider, List, ListItem, ListItemText } from "@mui/material";
+const style = {
+  width: "100%",
+  maxWidth: 360,
+  bgcolor: "background.paper",
+  marginLeft: "770px",
+};
+const ListData = (props) => {
+  return (
+    <List sx={style} component="nav" aria-label="mailbox folders">
+      <ListItem button>
+        <ListItemText primary={props.props.id} secondary="City ID" />
+      </ListItem>
+      <Divider />
+      <ListItem button divider>
+        <ListItemText primary={props.props.name} secondary="City Name" />
+      </ListItem>
+      <ListItem button>
+        <ListItemText
+          primary={props.props.main.temp}
+          secondary="City Temp &#8451;"
+        />
+      </ListItem>
+      <Divider light />
+      <ListItem button>
+        <ListItemText
+          primary={props.props.timezone}
+          secondary="City Timezone"
+        />
+      </ListItem>
+    </List>
+  );
+};
+
+export default ListData;
